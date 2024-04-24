2444404b8fda974dfed31e537ca558175c8cc6a5
Lordov <lidermtut1@gmail.com>
2024-04-24 11:03:48 +0300

    add redis_storage

--- __main__.py
+++ __main__.py
@@ -1,13 +1,15 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiogram.fsm.storage.redis import RedisStorage, DefaultKeyBuilder
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from redis.asyncio.client import Redis
+from redis.exceptions import ConnectionError
 
 from aiogram_dialog import setup_dialogs
-
-from environs import Env
 
 # from handlers.standart_handlers import router
 from tgbot.dialogs.Standart_dialog import start_dialog, prize_dialog
@@ -21,18 +23,57 @@ from tgbot.utils.commands import set_commands
 from tgbot.constants import BOT_TOKEN
 
 
-async def main():
+async def setup_dispatcher():
+    """
+    Функция для настройки диспетчера (Dispatcher).
+
+    Пытается установить соединение с Redis и создает хранилище данных
+    (RedisStorage) или, если соединение с Redis не удалось, использует
+    хранилище данных в памяти (MemoryStorage).
+
+    Returns:
+        Dispatcher: Объект диспетчера aiogram.
+    """
+
+    redis = Redis()
+    try:
+        await redis.ping()
+        storage = RedisStorage(
+            redis=redis, key_builder=DefaultKeyBuilder(with_destiny=True))
+    except ConnectionError:
+        print("Redis is not available, using MemoryStorage instead.")
+        storage = MemoryStorage()
+    dp = Dispatcher(storage=storage)
+    return dp
+
+
+async def setup_bot(dp: Dispatcher):
+    """
+    Функция для настройки бота (Bot).
+    Здесь же регистрируем роутеры и диалоги.
+
+    Args:
+        dp (Dispatcher): Объект диспетчера aiogram.
+
+    Returns:
+        Bot: Объект бота aiogram.
+    """
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(
         parse_mode=ParseMode.HTML))
-    await create_user_table()
-    # Инициализация бота
-    storage = MemoryStorage()
-    dp = Dispatcher(storage=storage)
     await set_commands(bot)
     dp.include_routers(*router_list)
     dp.include_routers(admin_panel, start_dialog, prize_dialog)
-
     setup_dialogs(dp)
+    return bot
+
+
+async def main():
+
+    dp = await setup_dispatcher()
+    bot = await setup_bot(dp)
+
+    await create_user_table()
+    await set_commands(bot)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
